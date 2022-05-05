6cdaf228f6121b6a34720cdbb27581cfa1b4cf07
yamamoto <ambmcmdmem@au.com>
2022-05-05 11:20:49 +0900

    [fix] AllowOriginの修正

--- config/app.php
+++ config/app.php
@@ -17,7 +17,7 @@ return [
      * Development Mode:
      * true: Errors and warnings shown.
      */
-    'debug' => filter_var(env('DEBUG', false), FILTER_VALIDATE_BOOLEAN),
+    'debug' => filter_var(env('DEBUG', true), FILTER_VALIDATE_BOOLEAN),
 
     /*
      * Configure basic information about the application.
@@ -421,6 +421,6 @@ return [
     ],
 
     'Cors' => [
-        'AllowOrigin' => ['http://localhost:3000']
+        'AllowOrigin' => ['https://ambmcmdmem.github.io']
     ]
 ];
