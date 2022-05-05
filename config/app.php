2894f38feaf95a8f029bcf9822a64dcaf22ad05e
yamamoto <ambmcmdmem@au.com>
2022-05-05 11:29:34 +0900

    [update] app.phpにテスト情報の追加

--- config/app.php
+++ config/app.php
@@ -17,7 +17,7 @@ return [
      * Development Mode:
      * true: Errors and warnings shown.
      */
-    'debug' => filter_var(env('DEBUG', true), FILTER_VALIDATE_BOOLEAN),
+    'debug' => filter_var(env('DEBUG', false), FILTER_VALIDATE_BOOLEAN),
 
     /*
      * Configure basic information about the application.
@@ -337,7 +337,8 @@ return [
             'className' => Connection::class,
             'driver' => Mysql::class,
             'persistent' => false,
-            'timezone' => 'UTC',
+            // 'timezone' => 'UTC',
+            'timezone' => 'Asia/Tokyo',
             //'encoding' => 'utf8mb4',
             'flags' => [],
             'cacheMetadata' => true,
@@ -421,8 +422,6 @@ return [
     ],
 
     'Cors' => [
-        'AllowOrigin' => [
-            'https://ambmcmdmem.github.io'
-        ]
+        'AllowOrigin' => ['https://ambmcmdmem.github.io']
     ]
 ];
